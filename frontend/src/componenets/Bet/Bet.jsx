194b23296b40bf0e2825110afed2f297f4369a79
vishal5208 <vishald5208@gmail.com>
2023-07-30 12:29:28 +0530

    background color

--- frontend/src/componenets/Bet/Bet.jsx
+++ frontend/src/componenets/Bet/Bet.jsx
@@ -147,104 +147,110 @@ const Bet = () => {
 
 	return (
 		<div className="">
-			<div>
-				<div className="w-1/3 mx-auto">
-					<p className="font-bold text-center text-3xl">
-						You can place a bet here
-					</p>
-					<form onSubmit={handleSubmit}>
-						<div className="grid sm:grid-cols-4 sm:gap-6 gap-3">
-							{/* Region */}
-							<div className="col-span-full flex flex-col space-y-2 justify-center">
-								<label
-									htmlFor="bet"
-									className="font-semibold sm:text-xl font-spaceGrotesk"
-								>
-									Bet
-								</label>
-								<select
-									id="bet"
-									className="bg-[#1A0142] text-white  uppercase border border-solid border-[#B1B1B1] rounded-lg sm:text-lg sm:p-4 p-2 sm:w-full"
-									value={bet}
-									onChange={(event) => setBet(event.target.value)}
-								>
-									<option value="">Select a bet</option>
-									<option value={true}>Heads</option>
-									<option value={false}>Tails</option>
-								</select>
-							</div>
+			<div className="w-1/3 mx-auto">
+				<p className="font-bold text-white text-center text-3xl mb-6">
+					Place Your Bet Now
+				</p>
+				<form onSubmit={handleSubmit} className="grid grid-cols-4 gap-3">
+					{/* Region */}
+					<div className="col-span-full flex flex-col space-y-2 justify-center">
+						<label htmlFor="bet" className="font-semibold text-white text-xl">
+							Bet
+						</label>
+						<select
+							id="bet"
+							className="bg-[#1A0142] text-white uppercase border border-solid border-[#B1B1B1] rounded-lg text-lg p-2"
+							value={bet}
+							onChange={(event) => setBet(event.target.value)}
+						>
+							<option value="">Select a bet</option>
+							<option value={true}>Heads</option>
+							<option value={false}>Tails</option>
+						</select>
+					</div>
 
-							{/* amount */}
-							<div className="col-span-full flex flex-col space-y-2  justify-center">
-								<label htmlFor="amount" className="font-semibold sm:text-xl">
-									Amount
-								</label>
-								<input
-									id="amount"
-									type="text"
-									pattern="\d*"
-									onInput={(event) => {
-										event.target.value = event.target.value.replace(/\D/g, "");
-									}}
-									className="bg-[#1A0142] text-white border border-solid border-[#B1B1B1] rounded-lg sm:text-lg sm:p-4 p-2 sm:w-full"
-									placeholder="Amount in $"
-									value={amount}
-									onChange={(event) => setAmount(event.target.value)}
-								></input>
-							</div>
+					{/* amount */}
+					<div className="col-span-full flex flex-col space-y-2 justify-center">
+						<label
+							htmlFor="amount"
+							className="font-semibold text-white text-xl"
+						>
+							Amount
+						</label>
+						<input
+							id="amount"
+							type="text"
+							pattern="\d*"
+							onInput={(event) => {
+								event.target.value = event.target.value.replace(/\D/g, "");
+							}}
+							className="bg-[#1A0142] text-white border border-solid border-[#B1B1B1] rounded-lg text-lg p-2"
+							placeholder="Amount in $"
+							value={amount}
+							onChange={(event) => setAmount(event.target.value)}
+						/>
+					</div>
 
-							<div className="sm:col-start-2 sm:col-span-2 ">
-								<button
-									type="submit"
-									className="text-white sm:text-2xl text-base font-semibold p-3 mt-2 rounded shadow bg-gradient-to-l  from-black to-purple-800 sm:py-2 sm:w-full"
-								>
-									Approve and Confirm
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
+					<div className="col-start-2 col-span-2 flex justify-center items-center">
+						<button
+							type="submit"
+							className="text-white font-semibold text-2xl py-2 px-4 rounded-lg bg-gradient-to-l from-black to-purple-800"
+						>
+							Approve and Confirm
+						</button>
+					</div>
+				</form>
 			</div>
 
-			<div className="mt-8 flex flex-col w-1/2 mx-auto">
-				<p className="font-bold text-3xl">
-					Congratulations to the winners of the last game!
-				</p>
-
-				{/* Show winners here */}
-				<div className="mt-4">
-					{winners.length > 0 ? (
-						<ul className="list-disc list-inside">
-							{winners.map((winner, index) => (
-								<li key={index} className="text-lg">
-									{winner}
-								</li>
-							))}
-						</ul>
+			<div className="mt-12 flex justify-between  w-11/12 mx-auto">
+				{/* Regarding time */}
+				<div className="text-white mt-4">
+					{gameStatus === 0 ? (
+						remainingTime !== null ? (
+							<p className="font-bold text-2xl">
+								Remaining Time: {formatTime(remainingTime)}
+							</p>
+						) : (
+							<p className="font-bold text-2xl text-lime-500">
+								Waiting for the First Player to Enter...
+							</p>
+						)
+					) : remainingTime === 0 ? (
+						<p className="font-bold text-2xl">
+							Results will be announced shortly!
+						</p>
 					) : (
-						<p className="text-lg">No winners yet.</p>
+						<p className="font-bold text-2xl">
+							The game is currently not open for betting.
+						</p>
 					)}
 				</div>
-			</div>
-			<div className="bg-blue-500 text-white px-4 py-2 mt-4 rounded-lg absolute top-16 right-2 m-2">
-				Game Status: {gameStatus === 0 ? "OPEN" : "CALCULATING"}
+
+				{/* last winners */}
+				<div className="flex flex-col">
+					<p className="font-bold text-white text-3xl">
+						Congratulations to the Winners of the Last Game!
+					</p>
+
+					{/* Show winners here */}
+					<div className="mt-4">
+						{winners.length > 0 ? (
+							<ul className="list-disc list-inside">
+								{winners.map((winner, index) => (
+									<li key={index} className="text-lg text-white">
+										{winner}
+									</li>
+								))}
+							</ul>
+						) : (
+							<p className="text-lg text-white">No winners yet.</p>
+						)}
+					</div>
+				</div>
 			</div>
 
-			<div>
-				{gameStatus === 0 ? (
-					remainingTime !== null ? (
-						<p>Remaining Time: {formatTime(remainingTime)}</p>
-					) : (
-						<p>
-							Currently awaiting the first player's entry to initiate the game
-							and start the timer.
-						</p>
-					)
-				) : remainingTime === 0 ? (
-					<p>The results will be announced shortly!</p>
-				) : (
-					<p>The game is currently not open for betting.</p>
-				)}
+			<div className="bg-blue-500 text-white px-4 py-2 mt-4 rounded-lg absolute top-16 right-2 m-2">
+				Game Status: {gameStatus === 0 ? "OPEN" : "CALCULATING"}
 			</div>
 		</div>
 	);
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
 		<div className="flex flex-col min-h-screen">
 			<Header />
 
-			<div className="flex-grow">
+			<div className="pt-28 flex-grow bg-gradient-to-r from-orange-800 to-black p-10">
 				<Bet />
 			</div>
 		</div>
